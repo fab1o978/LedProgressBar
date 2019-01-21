fe3fa76c53556ca1b4c295e069f6ebb7de9ab050
fab1o978 <info@fabiougolini.com>
2019-01-21 15:52:52 +0100

    Code cleanup

---
+++ examples/ThreeColors/ThreeColors.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
+#include <FastLED.h>
+#include <LedProgressBar.h>
+
+#define DATA_PIN 2
+#define NUM_LEDS 10
+
+LedProgressBar progressBar(NUM_LEDS);
+
+void setup() {
+
+  progressBar.setDebug(true);
+
+  // Set all leds to GREEN
+  progressBar.setColor(CRGB::Green);
+
+  // Set leds 6, 7 and 8 to ORANGE
+  progressBar.setColor(6, 8, CRGB::Orange);
+
+  // Set leds 9 and 10 to RED
+  progressBar.setColor(9, 10, CRGB::Red);
+
+  // Initialize FastLED object
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(progressBar.leds, NUM_LEDS);
+}
+
+void loop() {
+  delay(150);
+
+  progressBar.setProgress(random(0, 100));
+}
